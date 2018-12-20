fecc681d9daaf5b6a7cee56d23cb58035fa393bd
Xinzi Zhou <xzhou@indeed.com>
2018-12-20 10:45:53 +0800

    Select M1

--- device_proxy.py
+++ device_proxy.py
@@ -134,7 +134,8 @@ class DeviceProxy():
     def check_carrier(self):
         print 'Check carrier.'
         self.execute_mode('IncludeOk', 'AT+CNMP=2\r')
-        self.execute_mode('IncludeOk', 'AT+COPS=0\r')
+        # self.execute_mode('IncludeOk', 'AT+COPS=0\r')
+        self.execute_mode('IncludeOk', 'AT+COPS=1,2,"525503"\r') # Select M1 52503
         self.execute_mode('IncludeOk', 'AT+COPS?\r')
         self.execute_mode('IncludeOk', 'AT+COPS=?\r')
     
