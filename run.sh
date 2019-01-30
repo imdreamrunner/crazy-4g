c8585698e9a467ed4a34f75dd60f1a7c6e2c19b4
Xinzi Zhou <xzhou@indeed.com>
2019-01-30 12:33:15 +0800

    Change run script.

--- run.sh
+++ run.sh
@@ -1,3 +1,12 @@
 sleep 10
 export PYTHONUNBUFFERED=1
-python app.py > ./log-`date +%Y-%m-%d_%H:%M:%S`.txt 2>&1
+python app.py > ./log-`date +%Y-%m-%d_%H:%M:%S`.txt 2>&1 &
+
+while true; do
+    sleep 10
+    server=`ps aux | grep app.py | grep -v grep`
+    if [ ! "$server" ]; then
+        python app.py > ./log-`date +%Y-%m-%d_%H:%M:%S`.txt 2>&1 &
+        sleep 10
+    fi
+done
